fa7e6ef892147477eb8083c7645d6e6f185ded8e
Md. Hasan Ali <hasanmint@example.com>
2022-08-08 21:03:53 +0600

    intial setup react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Dashboard/Home';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/Dashboard/About';
 
 function App() {
   return (
     <div className="App">
       <h2>Welcome to Onion Dashboard</h2>
+      <h3>Welcome to React Router!</h3>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
